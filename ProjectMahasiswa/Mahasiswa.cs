3c832dbe25677d0d0623687d3104de5dee3063e4
klukman <lukman.arisandi@students.amikom.ac.id>
2022-07-19 13:21:24 +0700

    Add project files.

---
+++ ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa_Lukman
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+
+        public string Nama { get; set; }
+
+        public string JenisKelamin { get; set; }
+
+        public float IPK { get; set; }
+    }
+}
